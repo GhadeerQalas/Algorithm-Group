69fdc9e21180f3e151f0704dc598604436b3be4a
GhadeerQalas <ghadeer.balghaith18@gmail.com>
2020-03-29 20:43:38 +0300

    milestone #2

---
+++ Project/Milestone2/exercise8_4_1.js
@@ -0,0 +1,15 @@
+// graph from Exercise 1, section 8.4 (Levitan, 3rd edition)
+
+var _v = [ 
+	{label:"a"}, 
+	{label:"b"}, 
+	{label:"c"},
+	{label:"d"}
+	];
+	
+var _e = [
+	{u:0, v:1},
+	{u:1, v:2},
+	{u:2, v:3}
+	];
+
